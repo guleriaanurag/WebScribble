4c3e67c8b8114558590df9876e657929856a0784
Anurag Guleria <guleriaanurag84@gmail.com>
2024-01-26 02:36:17 +0530

    changes made to the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -21,14 +21,16 @@ export default function Header(){
     }
 
     return(
-        <header className="header sticky top-0 w-full h-[10%] bg-slate-800 flex justify-between py-2 px-6 max-lg:h-[15%]">
+        <header className="header sticky top-0 w-full h-[10%] z-[51] bg-slate-800 flex justify-between py-2 px-6 max-lg:h-[15%]">
             <label className="heading flex items-center justify-center text-4xl h-full"><Link to='/'> Web Scribbles </Link></label>
-            {isAuthenticated===false && (
-                <Link to='/login'><button className="px-5 outline-none bg-slate-100 rounded-xl text-lg h-12 max-lg:h-[60%] max-lg:my-auto">SignIn</button></Link>
+            <div className="flex items-center">
+                {isAuthenticated===false && (
+                    <Link to='/login'><button className="px-5 outline-none bg-slate-100 rounded-xl text-lg h-12 max-lg:h-[60%]">SignIn</button></Link>
+                    )}
+                {isAuthenticated===true && (
+                    <button className="px-5 outline-none flex items-center justify-center bg-slate-100 rounded-xl text-lg h-12 max-lg:h-[60%]" onClick={handleLogout}>Logout</button>
                 )}
-            {isAuthenticated===true && (
-                <button className="px-5 outline-none bg-slate-100 rounded-xl text-lg h-12 max-lg:h-[60%] max-lg:my-auto" onClick={handleLogout}>Logout</button>
-            )}
+            </div>
         </header>
     );
 }
