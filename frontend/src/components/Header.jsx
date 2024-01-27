92a020fe943a5b04ef43399534b70eaff545f4fa
Anurag Guleria <guleriaanurag84@gmail.com>
2024-01-27 18:38:42 +0530

    made header scroll responsive

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { AuthenticationContext } from "../store/AuthenticationContext";
 import Cookies from "js-cookie";
 import { toast } from "react-toastify";
+import Headroom from 'react-headroom'
 
 export default function Header(){
 
@@ -10,27 +11,29 @@ export default function Header(){
 
     function handleLogout(){
         Cookies.remove('authToken');
-        toast.error('User logged out',{
+        toast('User logged out',{
             pauseOnHover:false,
-            autoClose: 4000,
-            hideProgressBar: false,
-            theme: 'colored',
-            closeOnClick: true
+            autoClose: 3000,
+            hideProgressBar: true,
+            theme: 'light',
+            closeOnClick: true,
         });
         logoutUser();
     }
 
     return(
-        <header className="header sticky top-0 w-full h-[10%] z-[51] bg-slate-800 flex justify-between py-2 px-6 max-lg:h-[15%]">
-            <label className="heading flex items-center justify-center text-4xl h-full"><Link to='/'> Web Scribbles </Link></label>
-            <div className="flex items-center">
-                {isAuthenticated===false && (
-                    <Link to='/login' className="h-12 max-lg:h-[60%]"><button className="px-5 outline-none bg-slate-100 rounded-xl text-lg h-12 max-lg:h-full">SignIn</button></Link>
+        <Headroom>
+            <header className="header sticky top-0 w-full h-[10%] z-[51] bg-slate-800 flex justify-between py-2 px-6 max-lg:h-[15%]">
+                <label className="heading flex items-center justify-center text-4xl h-full"><Link to='/'> Web Scribbles </Link></label>
+                <div className="flex items-center">
+                    {isAuthenticated===false && (
+                        <Link to='/login' className="h-12 max-lg:h-[60%]"><button className="px-5 outline-none bg-slate-100 rounded-xl text-lg h-12 max-lg:h-full">SignIn</button></Link>
+                        )}
+                    {isAuthenticated===true && (
+                        <button className="px-5 outline-none bg-slate-100 rounded-xl text-lg h-12 max-lg:h-[60%]" onClick={handleLogout}>Logout</button>
                     )}
-                {isAuthenticated===true && (
-                    <button className="px-5 outline-none bg-slate-100 rounded-xl text-lg h-12 max-lg:h-[60%]" onClick={handleLogout}>Logout</button>
-                )}
-            </div>
-        </header>
+                </div>
+            </header>
+        </Headroom>
     );
 }
