bcdf64d6e85114f301557ff956757cf05b0f0e54
Anurag Guleria <guleriaanurag84@gmail.com>
2024-01-27 18:07:12 +0530

    added a footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <footer className="w-full h-auto p-8 bg-slate-800 text-slate-100 flex justify-between">
+            <p>&#169; 2024 Anurag</p>
+            <p>Contact me for addition of your desired category <a href="mailto:guleriaanurag84@gmail.com" className="text-sky-400 underline">here</a> </p>
+        </footer>
+    );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from "../components/Footer";
 
 export default function HomePage() {
     return (
@@ -9,6 +10,7 @@ export default function HomePage() {
             <Header />
             <ToastContainer/>
             <Outlet />
+            <Footer />
         </>
     )
 }
