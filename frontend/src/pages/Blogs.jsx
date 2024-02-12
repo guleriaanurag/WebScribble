e46274d448474be07ea1dcf7d6432a1171a83ad8
Anurag Guleria <guleriaanurag84@gmail.com>
2024-02-12 16:37:47 +0530

    Added a loader toast

--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -1,22 +1,23 @@
 import { Link, useLoaderData } from "react-router-dom";
 import axios from 'axios';
 import BlogCard from "../components/BlogCard";
+import { toast } from 'react-toastify';
 
-export default function Blogs(){
-    const {data} = useLoaderData();
+export default function Blogs() {
+    const { data } = useLoaderData();
 
-    return(
+    return (
         <div>
             <h3 className="heading text-3xl p-4 text-center">Blogs</h3>
             {data.length <= 0 && (
                 <>
-                <p className="text-center mt-24">No blogs available</p>
-                <p className="text-center"><Link to='post' className="text-sky-600 underline animate-pulse">Would you like to contribute</Link></p>
-            </>
+                    <p className="text-center mt-24">No blogs available</p>
+                    <p className="text-center"><Link to='post' className="text-sky-600 underline animate-pulse">Would you like to contribute</Link></p>
+                </>
             )}
             <div className="grid grid-cols-4 px-10 mt-6 pb-6 max-lg:grid-cols-2 max-md:grid-cols-1 max-md:px-3">
-                {data.map((d)=>{
-                    return <BlogCard key={d._id} data={d}/>
+                {data.map((d) => {
+                    return <BlogCard key={d._id} data={d} />
                 })}
             </div>
         </div>
@@ -25,6 +26,20 @@ export default function Blogs(){
 
 export async function loader(){
     const url = import.meta.env.VITE_BACKEND_URL;
+    console.log('loader called');
+    const myPromise = new Promise((resolve)=>
+        axios.get(url+'blogs')
+        .then((res)=> resolve(res.data))
+        .catch((err)=>resolve(err))
+    );
+    await toast.promise(myPromise,{
+        pending: 'Fetching Blogs Data',
+        success: 'Blogs Found',
+        error: 'There was an error fetching the blogs'
+    },{
+        pauseOnHover: false,
+        closeOnClick: true
+    })
     const response = await axios.get(url+'blogs');
     return response.data;
 }
