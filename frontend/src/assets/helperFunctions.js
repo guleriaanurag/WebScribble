fa0b16672766325b93a5235ec50412b76cc87851
Anurag Guleria <guleriaanurag84@gmail.com>
2024-04-26 12:24:39 +0530

    changes made to format function

--- frontend/src/assets/helperFunctions.js
+++ frontend/src/assets/helperFunctions.js
@@ -1,9 +1,8 @@
 export const formatTimeDifference = (commentCreatedAt) => {
     const now = new Date();
-    const commentDate = new Date(commentCreatedAt); // Assuming commentCreatedAt is a valid date string
-
+    const commentDate = new Date(commentCreatedAt);
     const differenceInMilliseconds = now.getTime() - commentDate.getTime();
-    const secondsInADay = 86400000; // 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
+    const secondsInADay = 24*60*60*1000;
 
     const years = Math.floor(differenceInMilliseconds / (secondsInADay * 365));
     const remainingMillisecondsAfterYears = differenceInMilliseconds % (secondsInADay * 365);
@@ -13,11 +12,11 @@ export const formatTimeDifference = (commentCreatedAt) => {
 
     let formattedDifference = '';
     if (years >= 1) {
-        formattedDifference += `${years} year${years > 1 ? 's' : ''}`; // Display year if >= 1
+        formattedDifference += `${years} year${years > 1 ? 's' : ''} ago`; // Display year if >= 1
     } else if (months > 1) {
-        formattedDifference += `${months} month${months > 1 ? 's' : ''}`; // Display months if > 1
+        formattedDifference += `${months} month${months > 1 ? 's' : ''} ago`; // Display months if > 1
     } else if (days > 0 && days < 30) {
-        formattedDifference += `${days} day${days > 1 ? 's' : ''}`; // Display days if less than 30
+        formattedDifference += `${days} day${days > 1 ? 's' : ''} ago`; // Display days if less than 30
     } else {
         formattedDifference = 'Just now';
     }
