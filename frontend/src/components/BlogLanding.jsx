709192ca5de8aff3395aaf87cb28cafbad6cbc30
Anurag Guleria <guleriaanurag84@gmail.com>
2024-04-28 16:50:28 +0530

    made image changes to the blog landing page

--- frontend/src/components/BlogLanding.jsx
+++ frontend/src/components/BlogLanding.jsx
@@ -23,6 +23,8 @@ export default function BlogLandingPage(){
     const[isEditable,setIsEditable] = useState(false);
     const navigate = useNavigate();
 
+    console.log(data.imageName);
+
     useEffect(()=>{
         let id=null;
         if(Cookies.get('authToken')!==undefined){
@@ -80,7 +82,7 @@ export default function BlogLandingPage(){
                 </div>
             )}
             {data.imageName && <img src={`${import.meta.env.VITE_BACKEND_URL}image/${data.imageName}`} alt={data.title} className="w-[80%] h-[80%] object-contain max-lg:object-fill max-md:w-[90%] max-md:h-[70%]"/>}
-            {!data.imageName && <img src={noImage} alt={data.title} className="w-[80%] h-[80%] object-contain max-lg:object-fill max-md:w-[90%] max-md:h-[70%]"/>}
+            {data.imageName==='' && <img src={noImage} alt={data.title} className="w-[80%] h-[80%] object-contain max-lg:object-fill max-md:w-[90%] max-md:h-[70%]"/>}
             <div className="blog-content mt-10 px-40 max-lg:px-5" dangerouslySetInnerHTML={{__html: sanitizedContent}}></div>
             <PostInteraction />
             <CommentWrapper comments={data.comments}/>
