f7676eee60abe74e6cfd10b6d76a2c2c88a786ba
Anurag Guleria <guleriaanurag84@gmail.com>
2024-05-08 12:25:10 +0530

    changed share modal style

--- frontend/src/components/ShareModal.jsx
+++ frontend/src/components/ShareModal.jsx
@@ -40,44 +40,30 @@ export default function ShareModal(){
         navigate(`/blogs/blog/${blogId}`);
     }
 
-    // const style = {
-    //     root: {
-    //         borderRadius: 3,
-    //         border: 0,
-    //         color: 'white',
-    //         height: '100%',
-    //         width: '100%',
-    //         margin: 'auto',
-    //         display: 'flex',
-    //         flexDirection: 'column',
-    //         justifyContent: 'space-evenly',
-    
-    //     },
-    //     copyContainer: {
-    //         border: '0px',
-    //         background: 'rgb(0,0,0,0.7)',
-    //     }
-    // };
+    const style = {
+        root: {
+            background: 'transparent',
+            borderRadius: 3,
+            border: 0,
+            color: 'white',
+        },
+        copyContainer: {
+            border: '1px solid blue',
+            background: 'rgb(0,0,0,0.7)'
+        },
+        title: {
+            color: 'aquamarine',
+            fontStyle: 'italic'
+        }
+    };      
 
-    
-    // const style = {
-    //     root: {
-    //         borderRadius: 3,
-    //         border: 0,
-    //         color: 'white',
-    //         height: '100%',
-    //         width: '100%',
-    //         margin: 'auto',
-    //         display: 'flex',
-    //         flexDirection: 'column',
-    //         justifyContent: 'space-evenly',
-    
     return (
         <dialog ref={shareModal} className="w-[40vw] h-[40vh] rounded-2xl max-lg:h-[43vh] max-lg:w-[80vw]" onKeyDown={(e)=>handleKeyDown(e)}>
             <button className="absolute top-4 right-4 p-2 outline-none text-2xl z-50" onClick={handleModalClose}>x</button>
             <ShareSocial 
                 url={window.location.origin+`/blogs/blog/${blogId}`}
                 socialTypes={['facebook','twitter','whatsapp','reddit','linkedin','email']}
+                style={style}
             />
         </dialog>
     );
